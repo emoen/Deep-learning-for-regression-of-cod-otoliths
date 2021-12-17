6c48f399407aa31b3b265cac18dc09873c8bebaa
Endre Moen <emoen@users.noreply.github.com>
2021-12-17 15:09:42 +0100

    10-fold - and 10% train-set size

--- Deep-learning-for-regression-of-cod-otoliths/Tensorflow/train_cod_from_jupyter.py
+++ Deep-learning-for-regression-of-cod-otoliths/Tensorflow/train_cod_from_jupyter.py
@@ -332,26 +332,28 @@ class CONFIG:
     valid_batch_size = 8
     img_size = 456
     val_img_size = 480
-    epochs = 1000
     learning_rate = 1e-5
     min_lr = 1e-6
     weight_decay = 1e-6
     T_max = 10
     scheduler = 'CosineAnnealingLR'
     n_accumulate = 1
-    n_fold = 5
+    n_fold_old1 = 5
+    n_fold = 10
     target_size = 1
     #device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     debugging = False
     which_exposure = 'min'
     CHANNELS = 'channels_last' #'channels_first' or 'channels_last' - pytorch or keras convention - needed by PIL
     KERAS_TRAIN_TEST_SEED = 2021
-    ROOTDIR = "./EFFNetB5_groupkfold_stdScalar/"
+    ROOTDIR_old1 = "./EFFNetB5_groupkfold_stdScalar/"
+    ROOTDIR = "./EFFNetB5_groupkfold_stdScalar_10_test/"
     CUDA_VISIBLE_DEVICE = "0"
     tensorboard_path = 'tensorboard_test2'
     checkpoint_path = 'checkpoints_test2/cod_oto_efficientnetBBB.{epoch:03d}-{val_loss:.2f}.hdf5'
     input_shape = (img_size, img_size, 3)
-    test_size = 0.15
+    test_size_old1 = 0.15
+    test_size = 0.1
     test_split_seed = 8
     steps_per_epoch = 1600
     epochs = 150
@@ -369,6 +371,7 @@ def dump_config_to_json( CONFIG ):
     config_dict.pop('__dict__', None)
     config_dict.pop('__weakref__', None)
     config_dict.pop('__doc__', None)
+    print("logging of config:"+str(CONFIG.ROOTDIR+'config.json'))
     with open(CONFIG.ROOTDIR + 'config.json', 'w', encoding='utf-8') as f:
         json.dump(config_dict, f, ensure_ascii=False, indent=4)
 
@@ -399,3 +402,4 @@ if __name__ == "__main__":
     df_final['test_pred_nn'] = test_predictions_nn
     df_final['y_true'] = test_age
     df_final.to_csv(CONFIG.ROOTDIR+'pred_mean_final.csv', index=False)
+
